627d8960d34c0f3f03314b2564722fa02456494e
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-08-23 13:14:06 +0530

    jj

--- src/service/createvehicle.js
+++ src/service/createvehicle.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/vehicles";
+const BASE_URL = "https://pallaku-backend.onrender.com/api/vehicles";
 
 export const createVehicle = async (formData) => {
-  // return await axios.post(`http://localhost:5000/create`, formData, {
-    return await axios.post(
-      `https://pallaku-backend.onrender.com/create`,
-    formData,
-    {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
+  try {
+    const response = await axios.post(`${BASE_URL}/create`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response;
+  } catch (error) {
+    console.error("Error creating vehicle:", error);
+    throw error;
+  }
 };
