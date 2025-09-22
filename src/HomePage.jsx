af6b75a73811582955f165fc6010ae94e69dde54
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-09-22 13:17:53 +0530

    timeout

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,12 @@
 // In HomePage.jsx or any protected page
 import { useEffect } from "react";
-import axios from "../utils/axiosInstance"; // adjust path if needed
+import AxiosInstance from "./axiosInstance";
 
 const HomePage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("/home"); // token will be auto-attached
+        const res = await AxiosInstance.get("/home"); // token will be auto-attached
         console.log(res.data);
       } catch (err) {
         console.error("Error fetching data:", err);
