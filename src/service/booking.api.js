3fc513044695c5484898c485a6ef7b7eda799d23
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-09-22 15:05:13 +0530

    timeout

--- src/service/booking.api.js
+++ src/service/booking.api.js
@@ -2,12 +2,19 @@
 import axios from "axios";
 
 export const fetchAllBookings = async () => {
+  const token = localStorage.getItem("token");
   try {
     // const response = await axios.get("http://localhost:5000/api/bookings", {
-    const response = await axios.get("https://pallaku-backend.onrender.com/api/bookings", {
-      headers: { "Content-Type": "application/json","Authorization": `Bearer ${localStorage.getItem("token")}` },
-      withCredentials: true, // ← include cookies
-    }); 
+    const response = await axios.get(
+      "https://pallaku-backend.onrender.com/api/bookings",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true, // ← include cookies
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Failed to fetch bookings:", error.message);
--- src/service/vehicles.js
+++ src/service/vehicles.js
@@ -4,7 +4,16 @@ const API_URL = "https://pallaku-backend.onrender.com/api/vehicles"; // Adjust p
 // const API_URL = "http://localhost:5000/api/vehicles"; // Adjust port if needed
 
 export const getAllVehicles = async () => {
-  const response = await axios.get(API_URL);
+  const token = localStorage.getItem("token");
+
+  const response = await axios.get(API_URL, {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // 👈 add token
+    },
+    withCredentials: true, // 👈 if backend also sets cookies
+  });
 
   return response.data;
 };
