93e8bd52a82ba43f6a48e77103e4a1fd37f01918
Tamilarasanps <tamiltamilarasan2530@gmail.com>
2025-09-22 13:43:18 +0530

    timeout

--- src/service/booking.api.js
+++ src/service/booking.api.js
@@ -4,16 +4,10 @@ import axios from "axios";
 export const fetchAllBookings = async () => {
   try {
     // const response = await axios.get("http://localhost:5000/api/bookings", {
-    const response = await axios.get(
-      "https://pallaku-backend.onrender.com/api/bookings",
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-        withCredentials: true, // ← include cookies
-      }
-    );
+    const response = await axios.get("https://pallaku-backend.onrender.com/api/bookings", {
+      headers: { "Content-Type": "application/json", },
+      withCredentials: true, // ← include cookies
+    }); 
     return response.data;
   } catch (error) {
     console.error("Failed to fetch bookings:", error.message);
